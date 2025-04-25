f0413f1559621cd45bf0fd137ae5e587f40671ee
rodrigmatrix <rodrigmatrix@gmail.com>
2025-04-24 23:34:56 -0300

    release 1.2.2

--- SubwayFreedomAssetPack/obj/Release/net472/SubwayFreedomAssetPack.AssemblyInfo.cs
+++ SubwayFreedomAssetPack/obj/Release/net472/SubwayFreedomAssetPack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SubwayFreedomAssetPack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60d55d071acb9fb39e430709c88a54a6cb3105f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7edf75342cb974639a805f82bcb16ffc2db4af4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SubwayFreedomAssetPack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SubwayFreedomAssetPack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
